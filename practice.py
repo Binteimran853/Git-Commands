7d83f13f66af3c2b9f250ad169d7d3a6be94b236
dev <binteimran853@gmail.com>
2026-03-02 11:23:51 +0500

    cherry-picking done

--- practice.py
+++ practice.py
@@ -5,9 +5,12 @@ def subtract(x, y):
     return x - y
 
 def multiply(x, y):
-    pass
+    return x * y
 
 def divide(x, y):
     return x//y
 
 print("hy")
+print(multiply(23, 4))
+print(multiply(34,45))
+print(divide(23, 4))
